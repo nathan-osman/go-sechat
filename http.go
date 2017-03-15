a3935856686460d991c86f9c2ae73c93f7d299ce
Nathan Osman <nathan@quickmediasolutions.com>
2017-03-15 15:40:09 -0700

    Fix request body bug in throttling code.

--- http.go
+++ http.go
@@ -33,34 +33,37 @@ func (c *Conn) newRequest(method, urlStr string, body io.Reader) (*http.Request,
 // time (to work around any throttle). Consequently, this method is blocking.
 func (c *Conn) postForm(path string, data *url.Values) (*http.Response, error) {
 	data.Set("fkey", c.fkey)
-	req, err := c.newRequest(
-		http.MethodPost,
-		fmt.Sprintf("https://chat.stackexchange.com%s", path),
-		strings.NewReader(data.Encode()),
-	)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	for {
+		req, err := c.newRequest(
+			http.MethodPost,
+			fmt.Sprintf("https://chat.stackexchange.com%s", path),
+			strings.NewReader(data.Encode()),
+		)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 		res, err := c.client.Do(req)
 		if err != nil {
 			return nil, err
 		}
-		if res.StatusCode >= 400 {
-			// For HTTP 409, wait for the throttle to cool down
-			if res.StatusCode == http.StatusConflict {
-				b, err := ioutil.ReadAll(res.Body)
-				if err == nil {
-					m := conflictRegexp.FindStringSubmatch(string(b))
-					if len(m) != 0 {
-						i, _ := strconv.Atoi(m[0])
-						c.log.Infof("retrying %s in %d second(s)", path, i)
-						time.Sleep(time.Duration(i) * time.Second)
+		// For HTTP 409, wait for the throttle to cool down
+		if res.StatusCode == http.StatusConflict {
+			b, err := ioutil.ReadAll(res.Body)
+			if err == nil {
+				m := conflictRegexp.FindStringSubmatch(string(b))
+				if len(m) != 0 {
+					i, _ := strconv.Atoi(m[0])
+					c.log.Infof("retrying %s in %d second(s)", path, i)
+					select {
+					case <-c.closeCh:
+					case <-time.After(time.Duration(i) * time.Second):
 						continue
 					}
 				}
 			}
+		}
+		if res.StatusCode >= 400 {
 			return nil, errors.New(res.Status)
 		}
 		return res, nil
